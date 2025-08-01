1365162a3d46ebd675306472778d09a60c3a6856
khaan <nguyenkhaan2006@gmail.com>
2025-08-01 08:18:37 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 function App()
 {
-  
+    return (
+        <div>
+            Xin chao cac ban
+        </div>
+    )
 }
 export default App
