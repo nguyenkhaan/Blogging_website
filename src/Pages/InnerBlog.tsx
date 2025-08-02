70b7ebc03221afd153ffc886e766918368354cbb
RinWasBusted <thaian0609asd@gmail.com>
2025-08-02 11:21:02 +0700

    Rebase Thai-Inner_Blog_page branch

--- src/Pages/InnerBlog.tsx
+++ src/Pages/InnerBlog.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import ContactBox from "../Component/ContactBox";
-import SideBar from "../Component/SideBar";
-import MailBox from "../Component/MailBox";
+import ContactBox from "../Component/InnerBlog/ContactBox";
+import SideBar from "../Component/InnerBlog/SideBar";
+import MailBox from "../Component/InnerBlog/MailBox";
 import Navbar from "../Component/Navbar";
 import Footer from "../Component/Footer";
 export default function InnerBlog() {
   return (
     <div className="w-full bg-[#F2F5F8] ">
-      { <Navbar></Navbar> }
+      {<Navbar></Navbar>}
       <main className="w-full grid grid-cols-16 ">
         <figure className="flex justify-center items-center overflow-hidden max-h-70 col-span-16 relative">
           <img src="/Image/background-slider-1.jpg" alt="banner-pic" />
@@ -145,7 +145,7 @@ export default function InnerBlog() {
           <ContactBox></ContactBox>
         </section>
       </main>
-      <Footer /> 
+      <Footer />
     </div>
   );
 }
