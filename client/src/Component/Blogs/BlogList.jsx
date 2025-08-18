94f7c8bfacaa7a66b750b22f4744ed0810ea3f34
khaan <nguyenkhaan2006@gmail.com>
2025-08-18 12:56:52 +0700

    them_ui_tong_hop_bai_viet

--- client/src/Component/Blogs/BlogList.jsx
+++ client/src/Component/Blogs/BlogList.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-function BlogList() 
-{
+import BlogItem from './BlogItem'
+function BlogList({blogs}) {
     return (
-        <div className = "flex-4 bg-blue-600 flex flex-col items-center justify-start">
-                Xin chao cac
+        <div className="px-6 lg:px-0 col-span-16 lg:col-span-12  min-h-80 gap-4 flex flex-col items-start justify-start divide-y divide-gray-300 pr-6">
+            <div className="flex w-full border-none text-xl items-center justify-end gap-4">
+                <i class="fa-solid fa-table-list" title="Chỉ tiêu đề"></i>
+                <i class="fa-solid fa-indent" title="Xem trước nội dung"></i>
+            </div>
+            {
+                blogs.map((blog) => <BlogItem blogData = {{...blog}} />)
+            }
             
         </div>
-    ) 
-} 
+    )
+}
 export default BlogList
--- client/src/Component/Blogs/RankingList.jsx
+++ client/src/Component/Blogs/RankingList.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-function RankingList() 
-{
+import AuthorItem from './AuthorItem'
+function RankingList({authors}) {
     return (
-        <div className = "flex-1 flex flex-col items-center justify-start">
-            Xin chao cac ban
-            <div className='bg-pink-200 w-100 h-100'></div>
-            <div className='bg-pink-200 w-100 h-100'></div>
-            <div className='bg-pink-200 w-100 h-100'></div>
-            <div className='bg-pink-200 w-100 h-100'></div>
-            <div className='bg-pink-200 w-100 h-100'></div>
-            <div className='bg-pink-200 w-100 h-100'></div>
+        <div className="col-span-4 hidden  lg:flex flex-col items-start justify-start gap-4 min-h-80">
+            <h3 className="text-xl text-blue-700">CÁC TÁC GIẢ HÀNG ĐẦU</h3>
+            <div className="w-full min-h-30">
+                {
+                    authors.map(author => <AuthorItem authorData = {{...author}} /> )
+                }
+            </div>
         </div>
     )
-} 
+}
 export default RankingList
