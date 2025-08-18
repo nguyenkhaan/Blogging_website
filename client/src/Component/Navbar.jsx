e502abca19db8227e9c53cf0684fed3e820eede1
khaan <nguyenkhaan2006@gmail.com>
2025-08-18 22:01:34 +0700

    fix_dang_nhap

--- client/src/Component/Navbar.jsx
+++ client/src/Component/Navbar.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import { useState } from 'react'
 import HorizontalStyle from './HorizontalStyle'
 import VerticalStyle from './VerticalStyle'
+import { motion } from 'framer-motion'
+import { Link } from 'react-router-dom'
 function NavbarAbove() {
     return (
         <div>
@@ -17,9 +19,17 @@ function NavbarAbove() {
                 <div className="flex-1 hidden items-center md:flex justify-end gap-3 text-lg">
                     <i class="fa-brands fa-facebook"></i>
                     <i class="fa-brands fa-square-instagram"></i>
-                    <i class="fa-brands fa-square-twitter"></i>
-                    <i class="fa-brands fa-square-x-twitter"></i>
-                    <i class="fa-solid fa-xmark ml-6"></i>
+                    <Link to = "/login">
+                        <motion.span
+                            className="text-lg font-semibold block hover:cursor-pointer hover:underline"
+                            initial={{ scale: 1 }}
+                            whileHover={{ scale: 1.1 }}
+                            transition={{ duration: 0.3, ease: "easeInOut" }}
+                        >
+                            Đăng nhập
+                        </motion.span>
+
+                    </Link>
                 </div>
             </div>
         </div>
@@ -29,13 +39,13 @@ function Navbar() {
     return (
         <div className="w-full static top-0 text-white text-base h-30 md:h-36">
             <NavbarAbove />
-            <div className = "w-full h-20 md:h-24">
-            {/*  Tùy vào loại màn hình mà chỉ một trong hai Horizontal Style và Vertical Style được phép hiển thị. Một bên là md:hiden thì bên kia md:block... */}
-                <HorizontalStyle />  
-                <VerticalStyle /> 
+            <div className="w-full h-20 md:h-24">
+                {/*  Tùy vào loại màn hình mà chỉ một trong hai Horizontal Style và Vertical Style được phép hiển thị. Một bên là md:hiden thì bên kia md:block... */}
+                <HorizontalStyle />
+                <VerticalStyle />
             </div>
         </div>
-/**Phai ho tro them bang Framer Motion de tao ra the div Doc  */
+        /**Phai ho tro them bang Framer Motion de tao ra the div Doc  */
     )
 }
 export default Navbar
